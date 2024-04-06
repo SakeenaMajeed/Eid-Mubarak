70b880668fdfba2239c1602aa4ca849efed601ff
sakeena majeed <160149270+SakeenaMajeed@users.noreply.github.com>
2024-04-06 23:00:57 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+import chalk from "chalk";
+console.log(chalk.cyan.bold("Eid mubarak to all my family members and friends!\n"));
+function eidMubarak() {
+    const eid = "Wishing you and your loved ones a joyous and blessed Eid. May this special occasion bring peace, happiness, and prosperity to your life.";
+    return eid;
+}
+let mubarak = eidMubarak();
+console.log(mubarak);
+function eidwish() {
+    const eid = "Best wishes,";
+    return eid;
+}
+let wish = eidwish();
+console.log(wish);
